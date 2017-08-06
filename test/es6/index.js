dca81544dc93799f5e70cf238ded0175668b79dd
Vidul Nikolaev Petrov <vidul.petrov@gmail.com>
2017-08-06 16:44:23 +0300

    Refactor unit tests - handle async assertions

--- test/es6/index.js
+++ test/es6/index.js
@@ -9,77 +9,111 @@ import {
     StepFive
 } from './react-components';
 
-it('Test basic functionality', () => {
+execute('Test basic functionality', label => {
     const component = shallow(<StepOne  />);
+    const compFalse = () => assert.equal(component.text(), 'S1 false');
+    const compTrue = () => assert.equal(component.text(), 'S1 true');
 
-    assert(component.text() === 'S1 false');
-
+    promiseIt(compFalse);
     component.find('div').simulate('click');
-    assert(component.text() === 'S1 true');
-
+    promiseIt(compTrue);
     component.find('div').simulate('click');
-    assert(component.text() === 'S1 false');
+    promiseIt(compFalse, label);
 });
 
-it('Test global storage', () => {
+execute('Test global storage', (label) => {
     const component = shallow(<StepTwo />);
+    const compFalse = () => assert.equal(component.text(), 'S2 false');
+    const compTrue = () => assert.equal(component.text(), 'S2 true');
 
-    assert(component.text() === 'S2 false');
-
+    promiseIt(compFalse);
     component.find('div').simulate('click');
-    assert(component.text() === 'S2 true');
-
+    promiseIt(compTrue);
     component.find('div').simulate('click');
-    assert(component.text() === 'S2 false');
+    promiseIt(compFalse, label);
 });
 
-it('Test callbacks', () => {
+execute('Test callbacks', label => {
     const component = shallow(<StepThree />);
+    const compFalse = () => assert.equal(component.text(), 'S3 false');
+    const compTrue = () => assert.equal(component.text(), 'S3 true');
 
-    assert(component.text() === 'S3 false');
-
+    promiseIt(compFalse);
     component.find('div').simulate('click');
-    assert(component.text() === 'S3 true');
-
+    promiseIt(compTrue);
     component.find('div').simulate('click');
-    assert(component.text() === 'S3 false');
+    promiseIt(compFalse, label);
 });
 
-it('Test promise success', () => {
+execute('Test promise success', label => {
     const component = shallow(<StepFour />);
+    const compareInitial = () => assert.equal(component.text(), '');
+    const compareFinal = () => assert.equal(component.find('div#success').text(), 'success');
 
-    assert(component.text() === '');
-
+    promiseIt(compareInitial);
     component.find('div#success').simulate('click');
-    setTimeout(() => assert(component.find('div#success').text() === 'success'));
+    promiseItAsync(compareFinal, label);
 });
 
-it('Test promise failure', () => {
+execute('Test promise failure', label => {
     const component = shallow(<StepFour />);
+    const compareInitial = () => assert.equal(component.text(), '');
+    const compareFinal = () => assert.equal(component.find('div#error').text(), 'error');
 
-    assert(component.text() === '');
-
+    promiseIt(compareInitial);
     component.find('div#error').simulate('click');
-    setTimeout(() => assert(component.find('div#error').text() === 'error'));
+    promiseItAsync(compareFinal, label);
 });
 
-it('Test on complete', () => {
+execute('Test on complete', label => {
     const component = shallow(<StepFive />);
+    const compareInitial = () => assert.equal(component.text(), '');
+    const compareFinal = () => assert.equal(component.find('div').text(), 'unsubscribed');
 
-    assert(component.text() === '');
-
+    promiseIt(compareInitial);
     component.find('div').simulate('click');
-    setTimeout(() => assert(component.find('div').text() === 'unsubscribed'));
+    promiseItAsync(compareFinal, label);
 });
 
-function it(string, fn) {
-    try {
-        fn();
-        console.log(colorGreenPass(), string);
-    }
-    catch (e) {
-        console.log(e);
-    }
+function promiseIt(...args) {
+    return promiseItSync(...args);
+}
+
+function promiseItSync(fn, label) {
+    promise(fn, false, label);
+}
+
+function promiseItAsync(fn, label) {
+    promise(fn, true, label);
+}
+
+function promise(fn, async, label) {
+    new Promise((resolve, reject) => {
+        const tryCatch = () => {
+            try {
+                fn();
+                resolve(label);
+            }
+            catch (e) {
+                reject(e);
+            }
+        };
+
+        if (async) {
+            setTimeout(tryCatch);
+        }
+        else {
+            tryCatch();
+        }
+    })
+    .then(
+        (s) => s && console.log(colorGreenPass(), s),
+        (e) => console.log(colorRed(e.name), e.message)
+    );
+};
+
+function execute(label, fn) {
+    fn(label);
 }
 
 function colorGreenPass() {
@@ -88,4 +122,8 @@ function colorGreenPass() {
 
 function colorGreen(text) {
     return `\x1b[42m\x1b[30m ${text} \x1b[0m`;
+}
+
+function colorRed(text) {
+    return `\x1b[41m\x1b[30m ${text} \x1b[0m`;
 }
